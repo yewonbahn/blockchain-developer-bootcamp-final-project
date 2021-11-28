c6fcc31a1771c4896a0a9266a8ee7c50faaefff1
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2021-11-28 23:17:16 +0900

    project front

--- src/App.js
+++ src/App.js
@@ -1,142 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import Modal from './Modal.js';
-import React, { useEffect, useState, useRef } from "react";
-import html2canvas from 'html2canvas';
-const App=() =>{
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => {
-    setModalOpen(true);
+import { Navbar } from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Home from './components/home/Home'
+// import PetDetails from './components/home-container/pet-details/PetDetails'
+import CreatePet from './components/CreateAvatar'
+
+
+import Web3 from 'web3'
+// import MyPet from './abis/Pet.json'
+import { useState } from 'react'
+
+
+function App() {
+  const [account, setAccount] = useState('')
+  const [contractData, setContractData] = useState('')
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying Metamask!',
+      )
+    }
   }
-  const closeModal = () => {
-    setModalOpen(false);
+
+  // const getContract = async () => {
+  //   const web3 = window.web3
+  //   const accounts = await web3.eth.getAccounts()
+  //   setAccount(accounts[0])
+  //   const networkId = await web3.eth.net.getId()
+  //   const networkData = MyPet.networks[networkId]
+  
+  //   if (networkData) {
+  //     const abi = MyPet.abi
+  //     const address = MyPet.networks[networkId].address
+  //     const myContract = new web3.eth.Contract(abi, address)
+  //     setContractData(myContract)
+  //   } else {
+  //     window.alert(
+  //       'Contract is not deployed to the detected network. Connect to the correct network!',
+  //     )
+  //   }
+  // }
+
+  const connectWallet = async () => {
+    await loadWeb3()
+    // await getContract()
   }
-  const [image, setImage] = useState('')
 
-  const [hash, setHash] = useState("0");
-
-const create=(e)=>{
-  var Buffer = require('buffer/').Buffer
-  var myImg;
-  e.preventDefault();
-  html2canvas(document.getElementById("backdrop"),{
-    scrollX: -window.scrollX,
-    scrollY: -window.scrollY,
-   
-  
-  }).then(function (canvas) {
-    myImg = canvas.toDataURL("image/png");
-  
-  
-    console.log(myImg);
-    var pngBuffer = Buffer(myImg);
-    console.log(pngBuffer);
-    const ipfsApi = require('ipfs-api');
-    const ipfs = ipfsApi('ipfs.infura.io', 5001, { protocol: "https" })
-    ipfs.files.add(pngBuffer, (err, result) => {// Upload buffer to IPFS
-      if (err) {
-        return "error";
-      }
-      console.log(result);
-      var fileHash = `${result[0].hash}`;
-      setHash(fileHash);
-      console.log(fileHash);
-      openModal();
- 
-    });
-   });
-}
-  let state = {
-    dress: 0,
-    hair: 0,
-    hat: 0,
-    shoes: 0,
-    accessory: 0,
-    face: 0,
-  };
   return (
-    <div id="box">
-    <div id="backdrop">
-      <div id="face"></div>
-      <div id="hat"></div>
-      <div id="hair"></div>
-      <div id="girl"></div>
-      <div id="shoes"></div>
-      <div id="dress"></div>
-    </div>
-    <div class="buttonBox">
-      <button id="nexthair" onClick={(e)=>{
-            let hair = document.querySelector("#hair");
-            if (state.hair < 5) {
-              state.hair++;
-              hair.setAttribute("class", `hair${state.hair}`);
-            } else if (state.hair === 5) {
-              state.hair = 0;
-              hair.setAttribute("class", `hair${state.hair}`);
-            }
-      }}>hair➡</button>
-
-
-      <button id="nextdress" onClick={(e)=>{
-           
-           let dress = document.querySelector("#dress");
-     console.log(dress);
-           if (state.dress < 7) {
-             state.dress++;
-             dress.setAttribute("class", `dress${state.dress}`);
-           } else if (state.dress === 7) {
-             state.dress = 0;
-             dress.setAttribute("class", `dress${state.dress}`);
-           }
-      }}>dress➡</button>
-
-
-      <button id="nextshoes" onClick={(e)=>{
-             let shoes = document.querySelector("#shoes");
-    if (state.shoes < 3) {
-      state.shoes++;
-      shoes.setAttribute("class", `shoes${state.shoes}`);
-    } else if (state.shoes === 3) {
-      state.shoes = 0;
-      shoes.setAttribute("class", `shoes${state.shoes}`);
-    }
-      }}>shoes➡</button>
-      <button id="nexthat" onClick={(e)=>{
-       let hat = document.querySelector("#hat");
-    if (state.hat < 4) {
-      state.hat++;
-      hat.setAttribute("class", `hat${state.hat}`);
-    } else if (state.hat === 4) {
-      state.hat = 0;
-      hat.setAttribute("class", `hat${state.hat}`);
-    }
-      }}>accessory➡</button>
-      <button id="nextface" onClick={(e)=>{
-           let face = document.querySelector("#face");
-    if (state.face < 2) {
-      state.face++;
-      face.setAttribute("class", `face${state.face}`);
-    } else if (state.face === 2) {
-      state.face = 0;
-      face.setAttribute("class", `face${state.face}`);
-    }
-      }}>face➡</button>
-    </div>
-    <div id="linksBox">
-      <a href="https://www.instagram.com/the_lonelymoongirl/"
-        ><img src="./images4/insta2.png" alt="insta" height="50px"
-      /></a>
-      <a href="https://www.amazon.co.uk/Lonely-Moongirl-Hazie/dp/1699887489"
-        ><img src="./images4/amazon.jpg" height="50px"
-      /></a>
-    </div>
-    <button onClick={create}>mint</button>
-    <Modal open={modalOpen} close={closeModal} header="" hash={hash}>
-          </Modal>
-        
-  </div>
-  );
+    <Router>
+      <div className="cl">
+      <Navbar account={account} connectWallet={connectWallet} />
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route exact path="/create-pet" component={CreatePet} />
+          <Route path="/pet-details/:petId">
+          {/* <PetDetails account={account} contractData={contractData} /> */}
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/CreateAvatar.js
@@ -0,0 +1,180 @@
+import logo from '../logo.svg';
+import './App.css';
+import Modal from '../Modal.js';
+import React, { useEffect, useState, useRef } from "react";
+import html2canvas from 'html2canvas';
+import { NFTStorage, File } from 'nft.storage';
+import { apiKey } from '../APIKEYS';
+
+const CreateAvatar=() =>{
+
+  const [loading, setLoading] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
+  const openModal = () => {
+    setModalOpen(true);
+  }
+  const closeModal = () => {
+    setModalOpen(false);
+  }
+  const [image, setImage] = useState('')
+
+  const [hash, setHash] = useState("0");
+
+// const create=(e)=>{
+//   var Buffer = require('buffer/').Buffer
+//   var myImg;
+//   e.preventDefault();
+//   html2canvas(document.getElementById("backdrop"),{
+//     scrollX: -window.scrollX,
+//     scrollY: -window.scrollY,
+   
+  
+//   }).then(function (canvas) {
+//     myImg = canvas.toDataURL("image/png");
+  
+  
+//     console.log(myImg);
+//     var pngBuffer = Buffer(myImg);
+//     console.log(pngBuffer);
+//     const ipfsApi = require('ipfs-api');
+//     const ipfs = ipfsApi('ipfs.infura.io', 5001, { protocol: "https" })
+//     ipfs.files.add(pngBuffer, (err, result) => {// Upload buffer to IPFS
+//       if (err) {
+//         return "error";
+//       }
+//       console.log(result);
+//       var fileHash = `${result[0].hash}`;
+//       setHash(fileHash);
+//       console.log(fileHash);
+//       openModal();
+ 
+//     });
+//    });
+// }
+
+const create=async (event) =>{
+  event.preventDefault()
+
+  var myImg;
+  event.preventDefault();
+  html2canvas(document.getElementById("backdrop"),{
+    scrollX: -window.scrollX,
+    scrollY: -window.scrollY,
+   
+  
+  }).then(async function (canvas) {
+    myImg = canvas.toDataURL("image/png");
+ 
+
+
+    try {
+      setLoading(true)
+      const client = new NFTStorage({ token: apiKey })
+      const metadata = await client.store({
+        name: "yewon",
+        description: "example",
+        image: new File([myImg], "", { type: "image/png" }),
+      })
+      if (metadata) {
+        console.log(metadata)
+      }
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    
+    }
+   });
+}
+  let state = {
+    dress: 0,
+    hair: 0,
+    hat: 0,
+    shoes: 0,
+    accessory: 0,
+    face: 0,
+  };
+  return (
+    <div id="box">
+    <div id="backdrop">
+      <div id="face"></div>
+      <div id="hat"></div>
+      <div id="hair"></div>
+      <div id="girl"></div>
+      <div id="shoes"></div>
+      <div id="dress"></div>
+    </div>
+    <div class="buttonBox">
+      <button id="nexthair" onClick={(e)=>{
+            let hair = document.querySelector("#hair");
+            if (state.hair < 5) {
+              state.hair++;
+              hair.setAttribute("class", `hair${state.hair}`);
+            } else if (state.hair === 5) {
+              state.hair = 0;
+              hair.setAttribute("class", `hair${state.hair}`);
+            }
+      }}>hair➡</button>
+
+
+      <button id="nextdress" onClick={(e)=>{
+           
+           let dress = document.querySelector("#dress");
+     console.log(dress);
+           if (state.dress < 7) {
+             state.dress++;
+             dress.setAttribute("class", `dress${state.dress}`);
+           } else if (state.dress === 7) {
+             state.dress = 0;
+             dress.setAttribute("class", `dress${state.dress}`);
+           }
+      }}>dress➡</button>
+
+
+      <button id="nextshoes" onClick={(e)=>{
+             let shoes = document.querySelector("#shoes");
+    if (state.shoes < 3) {
+      state.shoes++;
+      shoes.setAttribute("class", `shoes${state.shoes}`);
+    } else if (state.shoes === 3) {
+      state.shoes = 0;
+      shoes.setAttribute("class", `shoes${state.shoes}`);
+    }
+      }}>shoes➡</button>
+      <button id="nexthat" onClick={(e)=>{
+       let hat = document.querySelector("#hat");
+    if (state.hat < 4) {
+      state.hat++;
+      hat.setAttribute("class", `hat${state.hat}`);
+    } else if (state.hat === 4) {
+      state.hat = 0;
+      hat.setAttribute("class", `hat${state.hat}`);
+    }
+      }}>accessory➡</button>
+      <button id="nextface" onClick={(e)=>{
+           let face = document.querySelector("#face");
+    if (state.face < 2) {
+      state.face++;
+      face.setAttribute("class", `face${state.face}`);
+    } else if (state.face === 2) {
+      state.face = 0;
+      face.setAttribute("class", `face${state.face}`);
+    }
+      }}>face➡</button>
+    </div>
+    <div id="linksBox">
+      <a href="https://www.instagram.com/the_lonelymoongirl/"
+        ><img src="./images4/insta2.png" alt="insta" height="50px"
+      /></a>
+      <a href="https://www.amazon.co.uk/Lonely-Moongirl-Hazie/dp/1699887489"
+        ><img src="./images4/amazon.jpg" height="50px"
+      /></a>
+    </div>
+    <button onClick={create}>mint</button>
+    <Modal open={modalOpen} close={closeModal} header="" hash={hash}>
+          </Modal>
+        
+  </div>
+  );
+}
+
+export default  CreateAvatar;
