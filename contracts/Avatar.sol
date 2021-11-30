fcee7509a5ac25ea7a3d506bc19dfe6da6396327
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2021-12-01 02:09:58 +0900

    commit

--- contracts/Avatar.sol
+++ contracts/Avatar.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
+//Avatar is inherited from ERC721 contract
 contract Avatar is ERC721 {
-  constructor() ERC721("Avatar", "AVATAR") public  {}
+    //constructor has String Avatar
+  AggregatorV3Interface internal priceFeed;
+
+  constructor() ERC721("Avatar", "AVATAR") public  {
+       priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+  }
+
   event AvatarNFTCreated (
 
     uint tokenId,
@@ -19,4 +27,17 @@ contract Avatar is ERC721 {
        emit AvatarNFTCreated(_tokenId, _tokenURI, now, msg.sender);
     }
 
+    function looping (uint x) public returns (bool) {
+      for(uint i; i < x; i++){
+          loops += 1;
+      }
+      return true;
+  }
+  
+
  }
+   interface Token {
+    enum TokenType { Fungible, NonFungible }
+    struct Coin { string obverse; string reverse; }
+    function transfer(address recipient, uint amount) external;
+}
---
+++ design_pattern_decisions.md
@@ -0,0 +1,7 @@
+## Inheritance and Interfaces
+i use interface interface Token !
+Importing and extending contracts and/or using contract interfaces
+## Inheritance and Interfaces
+
+i use Restricting Access and Ownable
+ Using the modifier syntax, this specific function is owner, all other addresses will fail.
